39115d5c8530eb66f21e7f96e2608b66e3f61b25
chenxu <chenxu_2349@qq.com>
2023-05-15 21:02:01 +0800

    create sql script

--- src/main/java/com/example/laba1002/controller/HomeController.java
+++ src/main/java/com/example/laba1002/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
                         Model model) {
         // 在这里处理用户的登录逻辑，比如通过用户名和密码验证用户信息
         // 如果验证成功，可以跳转到其他页面，如果验证失败，可以返回登录页面并提示错误信息
-        return "index"; // 登录成功之后跳转的页面
+        return "home"; // 登录成功之后跳转的页面
     }
 
 }
